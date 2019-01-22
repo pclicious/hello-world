4237e2ea4d33fae9f0ca979fb8f996d5b7cfc83f
pclicious <31353699+pclicious@users.noreply.github.com>
2019-01-22 19:52:37 +0530

    Add files via upload

---
+++ circleprogress/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  formatSubtitle = (percent: number) : string => {
+    if(percent >= 100){
+      return "Congratulations!"
+    }else if(percent >= 50){
+      return "Half"
+    }else if(percent > 0){
+      return "Just began"
+    }else {
+      return "Not started"
+    }
+  }
+}
